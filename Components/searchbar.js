ab6f0836e94d630d1e2403c803f73ba1904916e3
abdurehman4 <arehmanimran4@gmail.com>
2025-12-21 10:27:21 +0500

    Text Padding

--- Components/searchbar.js
+++ Components/searchbar.js
@@ -8,14 +8,15 @@ class SEARCH extends HTMLElement {
         <div class="menu">
           <span class="material-symbols-outlined"> menu </span>
         </div>
-        <input type="text" placeholder="Search The Item" />
+        <input class='pd-20' type="text" placeholder="Search the Items" />
         <div class="mag">
           <i class="fa-solid fa-magnifying-glass"></i>
         </div>
-        <button id="categories">categories</button>
+        <button id="categories">Categories</button>
       </div>
       `;
   }
 }
 
 customElements.define("search-bar", SEARCH);
+
