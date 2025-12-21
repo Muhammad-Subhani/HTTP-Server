31bef1a9cf6e904b6b7ac51c9e3f2480067d7e47
Muhammad-Subhani <muhammadsubhani785@gmail.com>
2025-12-21 10:47:23 +0500

    added button

--- JS/carousle.js
+++ JS/carousle.js
@@ -4,6 +4,18 @@ const pics = document.querySelectorAll(".cdabba");
     function chalo() {
       screen.style.transform = `translateX(-${i * 100}%)`;
     }
+      const agla = document.getElementById("Forward");
+  const pichla = document.getElementById("Previous");
+  agla.addEventListener('click',()=>{
+    i++;
+    if(i>2) i=0;
+    chalo()
+  })
+  pichla.addEventListener('click',()=>{
+    i--;
+    if(i<0) i=0;
+    chalo()
+  })
     setInterval(() => {
       i++;
       if (i > 2) {
